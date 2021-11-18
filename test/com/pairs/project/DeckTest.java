bd057696f1a60a272026fe0662fa57d1abf6a3b0
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 15:21:55 +0000

    Test class for createDeck method

--- test/com/pairs/project/DeckTest.java
+++ test/com/pairs/project/DeckTest.java
@@ -1,4 +1,23 @@
+package com.pairs.project;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DeckTest {
-  
+    Deck deck;
+
+//    @BeforeEach
+    public  void init() {
+         deck = new Deck();
+    }
+
+   @Test
+    public void createDeckTest() {
+       deck = new Deck();
+       deck.getCards().stream().peek(System.out::println).toList();
+
+   }
 }

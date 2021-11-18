bb818889f95bab8de37dd534a0f42f310e12d1c7
frimps-astro <frimpsastro@gmail.com>
2021-11-18 11:50:19 +0000

    Added test functions to PlayGroundTest

--- src/test/PlayGroundTest.java
+++ src/test/PlayGroundTest.java
@@ -1,7 +1,12 @@
 package test;
-
-//import static org.junit.jupiter.api.Assertions.*;
+import com.pairs.project.PlayGround;
+import org.junit.jupiter.api.Test;
 
 class PlayGroundTest {
-
+    @Test
+    void testInitialShuffle(){
+        PlayGround play = new PlayGround();
+        System.out.println("hello");
+        play.initialShuffle();
+    }
 }

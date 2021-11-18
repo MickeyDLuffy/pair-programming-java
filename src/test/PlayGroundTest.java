a926a0839cb61b19a820bf6cf907cb6c2a24d642
frimps-astro <frimpsastro@gmail.com>
2021-11-18 12:17:49 +0000

    Added an Enum to store Card Values

--- src/test/PlayGroundTest.java
+++ src/test/PlayGroundTest.java
@@ -1,12 +1,17 @@
 package test;
+import com.pairs.project.CardType;
 import com.pairs.project.PlayGround;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
 
 class PlayGroundTest {
     @Test
     void testInitialShuffle(){
-        PlayGround play = new PlayGround();
-        System.out.println("hello");
-        play.initialShuffle();
+        //pick a card type at random
+        //pick a value related to that card at random
+        //assign it to a player
+        Stream.of(CardType.values())
+                .forEach(x->System.out.println(x));
     }
 }

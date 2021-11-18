21cd01083028704b8358595ec0600efeea3be38e
frimps-astro <frimpsastro@gmail.com>
2021-11-18 11:18:59 +0000

    Where the game is played

---
+++ src/com/pairs/project/PlayGround.java
@@ -0,0 +1,20 @@
+package com.pairs.project;
+
+public class PlayGround implements PlayStrategyInterface{
+    @Override
+    public void hit(){
+        
+    }
+
+    @Override
+    public void stick() {
+              
+    }
+
+    @Override
+    public void initialShuffle() {
+       CardType.values().stream()
+        
+    }
+    
+}

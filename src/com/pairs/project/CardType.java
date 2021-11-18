465e747948d0684186c64837f86e3882d49b7c74
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 15:20:02 +0000

    Added stream method to loop through the enum fields

--- src/com/pairs/project/CardType.java
+++ src/com/pairs/project/CardType.java
@@ -1,5 +1,11 @@
 package com.pairs.project;
 
+import java.util.stream.Stream;
+
 public enum CardType {
-   HEARTS, DIAMONDS, CLUBS, SPADES 
+   HEARTS, DIAMONDS, CLUBS, SPADES;
+
+   public static Stream<CardType> stream() {
+      return Stream.of(CardType.values());
+   }
 }
---
+++ src/com/pairs/project/CardValue.java
@@ -0,0 +1,28 @@
+package com.pairs.project;
+
+import java.util.stream.Stream;
+
+public enum CardValue {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10),
+    ACE(10);
+
+    private final int value;
+    CardValue(int value) {
+        this.value = value;
+    }
+
+    public static Stream<CardValue> stream() {
+        return Stream.of(CardValue.values());
+    }
+}

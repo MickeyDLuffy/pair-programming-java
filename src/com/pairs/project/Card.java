8e3eb4f07bbec7019d34405bd876e521a21e3361
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 15:20:46 +0000

    Added a card class to represent the cards within a deck

--- src/com/pairs/project/Card.java
+++ src/com/pairs/project/Card.java
@@ -1,7 +1,29 @@
 package com.pairs.project;
 
 public class Card {
-    private CardType suit;
-    private int value;
 
+
+    private final CardType suit;
+    private final CardValue value;
+
+    public Card(CardType suit, CardValue value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public CardType getSuit() {
+        return suit;
+    }
+
+    public CardValue getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", value=" + value +
+                '}';
+    }
 }

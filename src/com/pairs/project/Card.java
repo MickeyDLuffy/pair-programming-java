482e3352c8ea97e7e4422207ac932af942dc2a99
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 12:18:13 +0000

    Card class created

---
+++ src/com/pairs/project/Card.java
@@ -0,0 +1,7 @@
+package com.pairs.project;
+
+public class Card {
+    private CardType suit;
+    private int value;
+
+}
---
+++ src/com/pairs/project/Example.java
@@ -0,0 +1,10 @@
+package com.pairs.project;
+
+public enum Example {
+    TWO (2),
+    THREE(3);
+    private final int value;
+    Example(int value) {
+        this.value = value;
+    }
+}

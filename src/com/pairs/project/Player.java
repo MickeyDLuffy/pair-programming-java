61f6ef198cf6036921de77897d94a831f536a694
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 17:04:12 +0000

    Implemented strategy service for player

--- src/com/pairs/project/Player.java
+++ src/com/pairs/project/Player.java
@@ -1,14 +1,39 @@
 package com.pairs.project;
 
+import com.pairs.project.service.StrategyService;
+
 import java.util.List;
 
-public class Player {
+public class Player implements StrategyService {
     private Strategy strategy = Strategy.DEFAULT;
     private List<Card> cardsInHand;
 
-//    public int totalPoints() {
-//        cardsInHand.stream()
-//                .map(Card::getValue)
-//                .map(cardValue -> cardValue.)
-//    }
+    public Player(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+
+    public int totalPoints() {
+        if(cardsInHand.isEmpty()) {
+            return 0;
+        }
+        return cardsInHand.stream()
+                .map(Card::getValue)
+                .map(CardValue::getValue)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+
+    @Override
+    public void hit(Deck deck) {
+        if(totalPoints() < 17) {
+
+        }
+    }
+
+    @Override
+    public void stick(List<Card> cards) {
+
+    }
 }

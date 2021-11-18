6e92a47e18da6ce49395f37c7e6895909301b3ab
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 15:25:47 +0000

    Added iml to gitignore

--- src/com/pairs/project/Main.java
+++ src/com/pairs/project/Main.java
@@ -1,9 +1,17 @@
 package com.pairs.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        var n = new ArrayList<>();
+        CardValue.stream().map(type -> n.add(type.name()));
+        System.out.println(n);
 	// write your code here
 //      PlayGround playGround = n
+//        Deck deck = new Deck(List.of(new Card(CardType.DIAMONDS, CardValue.FOUR)));
+//        CardValue.stream().map(Enum::name).peek(System.out::println).toList();
     }
 }

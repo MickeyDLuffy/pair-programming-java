a1308f56e984a91207d15157223a14f28c6ff291
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-18 17:07:43 +0000

    STrategy service

--- src/com/pairs/project/service/StrategyService.java
+++ src/com/pairs/project/service/StrategyService.java
@@ -1,11 +1,12 @@
 package com.pairs.project.service;
 
 import com.pairs.project.Card;
+import com.pairs.project.Deck;
 
 import java.util.List;
 
 public interface StrategyService {
-    public void hit(List<Card> cards);
+    public void hit(Deck deck);
     public void stick(List<Card> cards);
     public default void goBust() {
 

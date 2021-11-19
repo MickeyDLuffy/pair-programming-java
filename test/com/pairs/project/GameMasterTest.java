4bab02c94a0836bf56cb46c4648bdce4e6289838
frimps-astro <frimpsastro@gmail.com>
2021-11-19 12:56:37 +0000

    Added test for players creation and strategy testing

--- test/com/pairs/project/GameMasterTest.java
+++ test/com/pairs/project/GameMasterTest.java
@@ -1,19 +1,38 @@
 package com.pairs.project;
 
-import com.pairs.project.CardType;
-import com.pairs.project.PlayGround;
 import org.junit.jupiter.api.Test;
 
 import java.util.stream.Stream;
 
 
-class PlayGroundTest {
+class GameMasterTest {
+    Deck deck = new Deck();
+
+    GameMaster gameMaster = new GameMaster(deck);
+
     @Test
-    void testInitialShuffle(){
+    void testInitialShuffle() {
         //pick a card type at random
         //pick a value related to that card at random
         //assign it to a player
         Stream.of(CardType.values())
-                .forEach(x->System.out.println(x));
+                .forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    public void initPlayers() {
+        gameMaster.getPlayers().stream().forEach(System.out::println);//does it print show players and their two cards
+    }
+
+    @Test
+    public void testApplyStrategy() {
+        gameMaster.applyStrategy();
+        System.out.println("==============================================================\n");
+        gameMaster.getPlayers().stream().forEach(System.out::println);//does it print show players and their two cards
+    }
+
+    @Test
+    public void testPlayGame(){
+
     }
 }
